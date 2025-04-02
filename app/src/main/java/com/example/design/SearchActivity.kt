fcdfdcf2f1c6f46b88e8534063143eb47e50d061
soumyajitnandi0 <soumyajitnandi7384@gmail.com>
2025-04-02 15:09:30 +0530

    update

--- app/src/main/java/com/example/design/SearchActivity.kt
+++ app/src/main/java/com/example/design/SearchActivity.kt
@@ -2,6 +2,7 @@ package com.example.design
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()  // Add this line
         setContentView(R.layout.activity_search)
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
@@ -28,5 +30,9 @@ class SearchActivity : AppCompatActivity() {
                 else -> false
             }
         }
+
+        findViewById<ImageButton>(R.id.back_button).setOnClickListener {
+            finish()
+    }
     }
 }
